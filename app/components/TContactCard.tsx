667f3551c6ac9b090782ceee658b130334df75f6
Pulok <pulok@property.credit>
2024-05-22 17:59:05 +1000

    Firebase integration and image upload feature

--- app/components/TContactCard.tsx
+++ app/components/TContactCard.tsx
@@ -23,9 +23,6 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import Link from "@mui/material/Link";
 import { TransitionProps } from "@mui/material/transitions";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import CallIcon from "@mui/icons-material/Call";
-import EmailIcon from "@mui/icons-material/Email";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 import HouseIcon from "@mui/icons-material/House";
 import React from "react";
@@ -37,6 +34,10 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import CardContentDetails from "./CardContentDetails";
+import TContactDetails from "./TContactDetails";
+import TContactImage from "./TContactImage";
+import Image from "next/image";
 
 type TContactCardProps = {
   contact: Contact;
@@ -56,6 +57,8 @@ const TContactCard = ({ contact, currentUser }: TContactCardProps) => {
   const [contactExists, setContactExists] = useState<boolean>(false);
   const checkExistence = currentUser?.favourites.includes(contact.id);
   const [open, setOpen] = useState(false);
+  const [openDropzone, setOpenDropzone] = useState(false);
+  console.log(contact.name + " is" + contact.image);
 
   const handleFavorites = async () => {
     axios
@@ -85,154 +88,59 @@ const TContactCard = ({ contact, currentUser }: TContactCardProps) => {
   const handleClose = () => {
     setOpen(false);
   };
+  const handleDropzoneClose = () => {
+    setOpenDropzone(false);
+  };
+  const handleDropzoneOpen = () => {
+    setOpenDropzone(true);
+  };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height={80}
-          image="/avatar1.jpg"
-          alt="green iguana"
-        />
-        {/* <div className="mt-5 ml-12">
-          <Avatar src="/avatar1.jpg" />
-        </div> */}
+    // sx={{ maxWidth: 345 }}
+    // className="max-w-m flex flex-col justify-between"
+    <Card
+      sx={{ width: 345 }}
+      className="max-w-xs flex flex-col justify-between"
+    >
+      <CardActionArea
+        onClick={() => {
+          handleDropzoneOpen();
+        }}
+      >
+        <div className="aspect-square overflow-hidden relative w-full">
+          {contact.image ? (
+            <Image
+              fill
+              alt={"name"}
+              src={contact.image}
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <Image
+              fill
+              alt={"name"}
+              src={"/avatar1.jpg"}
+              className="w-full h-full object-contain"
+            />
+          )}
+        </div>
+
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {contact.name}
-          </Typography>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AccountCircleIcon />
-            <p style={{ marginLeft: "8px" }}>{contact.username}</p>
-          </div>
-          <Divider className="mt-3" />
-          <div
-            style={{ display: "flex", alignItems: "center", marginTop: "3px" }}
-          >
-            <CallIcon />
-            <p style={{ marginLeft: "8px" }}>{contact.phone}</p>
-          </div>
-          <Divider className="mt-3" />
-          <div
-            style={{ display: "flex", alignItems: "center", marginTop: "3px" }}
-          >
-            <EmailIcon />
-            <p style={{ marginLeft: "8px" }}>{contact.email}</p>
-          </div>
+          <CardContentDetails contact={contact} />
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions className="mt-auto flex justify-between">
         <Button size="small" color="primary" onClick={handleClickOpen}>
           View
         </Button>
-        <Dialog
-          open={open}
-          TransitionComponent={Transition}
-          keepMounted
-          onClose={handleClose}
-          aria-describedby="alert-dialog-slide-description"
-          maxWidth="md"
-          fullWidth
-          PaperProps={{
-            sx: {
-              width: "400px",
-              height: "300px",
-            },
-          }}
-          //   className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black"
-        >
-          <DialogTitle
-            className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black"
-            // style={{ border: "2px solid black", borderRadius: "0.1px" }}
-          >
-            {`${contact.name}'s Details`}
-          </DialogTitle>
-          <Divider className="bg-black" />
-          <div className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black">
-            <DialogContent>
-              <DialogContentText id="alert-dialog-slide-description">
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginTop: "3px",
-                  }}
-                >
-                  <TravelExploreIcon />
-                  {/* <p style={{ marginLeft: "8px" }}>{contact.website}</p> */}
-                  <Link
-                    href="#"
-                    underline="always"
-                    color={"#nnn"}
-                    className="ml-4"
-                  >
-                    {contact.website}
-                  </Link>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginTop: "6px",
-                  }}
-                >
-                  <HouseIcon />
-                  <Typography variant="body1" className="ml-4">
-                    {contact.address.suite}, {contact.address.street},{" "}
-                    {contact.address.city}, {contact.address.zipcode}
-                  </Typography>
-                </div>
-                <Divider className="bg-black" />
-                <div className="mt-5 ">
-                  <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel1-content"
-                      id="panel1-header"
-                      className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black"
-                    >
-                      {contact.company.name}
-                    </AccordionSummary>
 
-                    <AccordionDetails className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black">
-                      <Divider className="bg-black " variant="fullWidth" />
-                      <div
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                          marginTop: "8px",
-                        }}
-                      >
-                        <RecordVoiceOverIcon />
-                        <p style={{ marginLeft: "8px" }}>
-                          {contact.company.catchPhrase}
-                        </p>
-                      </div>
-                      <Divider className="bg-black mt-3" variant="fullWidth" />
-                      <div
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                          marginTop: "10px",
-                        }}
-                      >
-                        <BusinessCenterIcon />
-                        <p style={{ marginLeft: "8px" }}>
-                          {contact.company.bs}
-                        </p>
-                      </div>
-                    </AccordionDetails>
-                  </Accordion>
-                </div>
-              </DialogContentText>
-            </DialogContent>
-          </div>
-          <DialogActions className="bg-gradient-to-r from-gray-200 to-gray-500 bg-cover bg-center text-black">
-            <Button onClick={handleClose}>Close</Button>
-          </DialogActions>
-        </Dialog>
         <Button size="small">Add to Favourites</Button>
+        <TContactDetails contact={contact} open={open} onClose={handleClose} />
+        <TContactImage
+          contact={contact}
+          open={openDropzone}
+          onClose={handleDropzoneClose}
+        />
       </CardActions>
     </Card>
   );
